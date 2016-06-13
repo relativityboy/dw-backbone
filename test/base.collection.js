6d0850c259ce1188d7528e3c6f74d64b4c617b42
Donovan Walker <donovan@donovanwalker.com>
2016-06-13 18:22:46 -0500

    Re-adding collections tests

---
+++ test/base.collection.js
@@ -0,0 +1,106 @@
+var Backbone  = require('backbone');
+var Base      = require('../src/base');
+var expect    = require("chai").expect;
+var _         = require('underscore');
+
+
+
+describe('Base.Collection', function() {
+  var Model, Collection, collection, errMap, keyMap, modelsJSON, modelJSON, counter, undef;
+  before(function() {
+    Model = Base.Model.extend({
+      jsonMaps:{
+        api:{
+          from:{
+            convert:'toCamel'
+          },
+          to:{
+            convert:'toUnderscored'
+          }
+        }
+      },
+      incCounter:function() {
+      counter++;
+    }
+    });
+
+    Collection = Base.Collection.extend({
+      model:Model
+    });
+  });
+  beforeEach(function() {
+    counter = 0;
+    errMap = [];
+    keyMap = {'firstName':'first_name', 'lastName':'last_name', 'address':'address', 'city':'city'};
+    modelsJSON = [
+      {
+        first_name:'George',
+        last_name:'Menlow',
+        address:'1785 Thurston St',
+        city:'VanCouver'
+      },
+      {
+        first_name:'Obama',
+        last_name:'Barack',
+        address:'1700 Whitehouse St.',
+        city:'Washington'
+      },
+      {
+        first_name:'Arnold ',
+        last_name:'Schwartzenwhatever',
+        address:'Hummer Drive',
+        city:'Calicopia'
+      }
+    ];
+
+    modelJSON = {
+      first_name:'David',
+      last_name:'Attenborough',
+      address:'Staged Authenticity',
+      city:'Tree Town'
+    }
+    collection = new Collection(modelsJSON, 'api')
+  });
+  it("exists", function() {
+    expect(typeof Base.Collection).to.equal('function')
+  });
+  it("is instantiable", function() {
+    collection = new Base.Collection();
+    expect(typeof collection).to.equal('object')
+  });
+  it("supports jsonMap Model constructor modes", function() {
+    collection.each(function(model, i) {
+      _.each(keyMap, function(val, key) {
+        if(!model.attributes.hasOwnProperty(key)) {
+          expect(key).to.equal(val);
+        }
+      });
+    });
+  });
+  it(".toJSON supports jsonMap Model toJSON modes", function() {
+    var jsonList = collection.toJSON('api');
+    _.each(jsonList, function(json, i) {
+      _.each(keyMap, function(val, key) {
+        if(!json.hasOwnProperty(val)) {
+          expect(val).to.equal(key);
+        }
+      });
+    });
+  });
+  it(".setOnAll supports jsonMap Model toJSON modes", function() {
+    collection.setOnAll('planet', 'earth');
+    collection.each(function(model, i) {
+      expect('earth').to.equal(model.attributes.planet);
+    });
+  });
+  it(".callOnAll supports jsonMap Model toJSON modes", function() {
+    collection.callOnAll('incCounter');
+    expect(counter).to.equal(3)
+  });
+  it(".unsetOnAll supports jsonMap Model toJSON modes", function() {
+    collection.unsetOnAll('firstName');
+    collection.each(function(model, i) {
+      expect(model.get('firstName')).to.equal(undef);
+    });
+  });
+});
